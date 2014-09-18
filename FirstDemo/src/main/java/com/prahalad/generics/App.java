400814080b0fec5fc316ef27d60370fdebda7128
kothwal <pkothwal@gmail.com>
2014-09-18 14:16:36 +0530

    changed the sysout message

---
+++ FirstDemo/src/main/java/com/prahalad/generics/App.java
@@ -0,0 +1,13 @@
+package com.prahalad.generics;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hey You are learning GIT !!" );
+    }
+}
